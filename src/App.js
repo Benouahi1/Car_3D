9fdabb1a3d291bcf6b6da45192bfd9aae8f85a90
ayoub benouahi <abenouahi5@gmail.com>
2022-12-26 13:36:50 +0100

    Car 3d

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/bmw.glb");
+  return <primitive object={scene} {...props} />
+}
+
+function App() {
+  return (
+    <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
+      <color attach="background" args={["#101010"]} />
+      <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+        <Stage environment={"sunset"}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
+  );
+}
+
+export default App;
